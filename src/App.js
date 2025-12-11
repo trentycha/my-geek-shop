429be5faecd0b23ae978f7cb0bfbd215a12d8133
trentycha <trentycharlene@gmail.com>
2025-12-11 21:46:45 +0100

    API's user list added

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home.js';
 import Login from './pages/Login.js';
+import UserList from './pages/UserList.js';
 import Header from './components/Header.js';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/userlist" element={<UserList />} />
       </Routes>
     </div>
   );
